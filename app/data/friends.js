e4dec9f818e639e915e0ec0cad8f74ea43305012
Jimmie Jackson <jimmiejackson414@gmail.com>
2016-07-21 22:25:51 -0500

    making progress

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,3 +14,5 @@ var friendsData = [{
         1
     ]
 }]
+
+module.exports = friendsData;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,7 +1,3 @@
-// Your api-routes.js file should contain two routes:
-
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
 //////////////////
 // DEPENDENCIES //
@@ -17,14 +13,13 @@ var path = require('path');
 
 module.exports = function(app){
 
-	app.get('api/friends', function(req, res){
+	app.get('/api/friends', function(req, res){
 		res.json(friendsData);
-		res.json(true);
 	});
 
 	app.post('/api/friends', function(req, res){
 		res.json(friendsData);
-		res.json(true);
+		// res.json(true);
 	});
 
 	// app.post('/api/clear', function(req, res){
