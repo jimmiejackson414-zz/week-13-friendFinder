6c9a394c6e4ace40f6161473f831c6e4d46a482c
Jimmie Jackson <jimmiejackson414@gmail.com>
2016-07-23 15:05:16 -0500

    final commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,35 @@
 var friendsData = [{
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-}]
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    } {
+        name: "Jimmie",
+        photo: 'https://www.dropbox.com/s/bzp7rumk1dg10n3/selfie_day176.jpg?dl=0',
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    }
+
+]
 
 module.exports = friendsData;
